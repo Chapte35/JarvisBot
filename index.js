c99d9c926a5a41c531cb054ae5e15b3f047de9f9
Chapte35 <valbuk2001@gmail.com>
2025-05-06 14:14:34 +0200

    début commentaires

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+//Dépendances vers d'autres fichiers
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.commands = new Collection();
-
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
+
+//Initialisation de la liste de commandes du bot client
+client.commands = new Collection();
+
+//Récupération des fichiers de commandes (finissant par .js)
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		//Ajoute une commande a la liste de commandes avec le nom de commande pour clé et le module exporté en valeur
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
